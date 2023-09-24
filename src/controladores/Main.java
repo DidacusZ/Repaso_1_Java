04314dce03a2002cfdc9381b44d2055748c2a477
DESKTOP-5TCD4T0\EQUIPO <f>
2023-09-24 20:20:24 +0200

    24/09/23

--- src/controladores/Main.java
+++ src/controladores/Main.java
@@ -15,6 +15,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//prueba ++++ ----
+		
+		
 		// implementaciones
 		InterfaceMenu implMenu = new ImplMenu();
 		InterfaceTrabajador implTrabajador = new ImplTrabajador();
